5177eec7308a0b269dd9e7b4956c592cd5c95958
Tanmeet Kaur <tanmeet@Tanmeets-MacBook-Air.local>
2024-08-03 10:30:39 +0530

    initial commit

---
+++ Spring-Guide/src/test/java/com/example/Spring/Guide/AllSpringGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSpringGuidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ initial/src/main/java/com/example/Spring/Guide/AllSpringGuidesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSpringGuidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllSpringGuidesApplication.class, args);
+	}
+
+}
